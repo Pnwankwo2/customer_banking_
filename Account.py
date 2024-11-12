cb2d3d16de0fa499fc83277942646e029f4721ed
PNwankwo2 <patrick.nwankwo764@gmail.com>
2024-11-12 07:25:25 -0500

    customer_banking Complete

---
+++ Account.py
@@ -0,0 +1,26 @@
+""" Create a Account class with methods"""
+
+class Account:
+    class Account:
+     """Creating an Account class with methods"""
+    
+    def __init__(self, balance, interest):
+        """Initializes the account with a balance and interest"""
+        self.balance = balance
+        self.interest = interest
+
+    def set_balance(self, balance):
+        """Sets the balance for the account"""
+        self.balance = balance
+
+    def set_interest(self, interest):
+        """Sets the interest gained for the account"""
+        self.interest = interest
+
+    def get_balance(self):
+        """Returns the current balance of the account"""
+        return self.balance
+
+    def get_interest(self):
+        """Returns the current interest of the account"""
+        return self.interest
